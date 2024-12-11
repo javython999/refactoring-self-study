4dcc21d4aa1a2d57c1c231a19525c97dc1a2a7cd
javython999 <infinity2544@gmail.com>
2024-12-11 20:04:11 +0900

    Play, Performance class

--- statement.js
+++ statement.js
@@ -1,79 +1,108 @@
-export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+export function statement(invoice) {
+
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+  let totalAmount = 0;
+  let detail = "";
+  for (let performance of invoice.performances) {
+    const play = performance.play;
     let thisAmount = 0;
 
     switch (play.type) {
       case 'tragedy': // 비극
         thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+        if (performance.audience > 30) {
+          thisAmount += 1000 * (performance.audience - 30);
         }
         break;
       case 'comedy': // 희극
         thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+        if (performance.audience > 20) {
+          thisAmount += 10000 + 500 * (performance.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        thisAmount += 300 * performance.audience;
         break;
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
-
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    totalAmount += thisAmount;
 
     // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += thisAmount;
+    detail += `  ${play.name}: ${format(thisAmount / 100)} (${performance.audience}석)\n`;
+    
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
+
+  
+  // 포인트를 적립한다.
+  let volumeCredits = 0;
+  for (let performance of invoice.performances) {
+    const play = performance.play;
+    volumeCredits += Math.max(performance.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === play.type) volumeCredits += Math.floor(performance.audience / 5);
+  }
+
+  
+  function getResult(customer, detail, totalAmount, volumeCredits) {
+    return `청구 내역 (고객명: ${customer})\n${detail}총액: ${totalAmount}\n적립 포인트: ${volumeCredits}점\n`;
+  }
+
+  return getResult(invoice.customer, detail, format(totalAmount / 100), volumeCredits);
 }
 
 // 사용예:
-const playsJSON = {
-  hamlet: { name: 'Hamlet', type: 'tragedy' },
-  'as-like': { name: 'As You Like It', type: 'comedy' },
-  othello: { name: 'Othello', type: 'tragedy' },
-};
+class Play {
+  #name;
+  #type;
+
+  constructor(name, type) {
+    this.#name = name;
+    this.#type = type;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get type() {
+    return this.#type;
+  }
+}
+
+class Performance {
+  #play;
+  #audience;
+
+  constructor(play, audience) {
+    this.#play = play;
+    this.#audience = audience;
+  }
+
+  get play() {
+    return this.#play;
+  }
+
+  get audience() {
+    return this.#audience;
+  }
+}
 
 const invoicesJSON = [
   {
     customer: 'BigCo',
     performances: [
-      {
-        playID: 'hamlet',
-        audience: 55,
-      },
-      {
-        playID: 'as-like',
-        audience: 35,
-      },
-      {
-        playID: 'othello',
-        audience: 40,
-      },
+      new Performance(new Play('Hamlet', 'tragedy'), 55),
+      new Performance(new Play('As You Like It', 'comedy'), 35),
+      new Performance(new Play('Othello', 'tragedy'), 40),
     ],
   },
 ];
 
-const result = statement(invoicesJSON[0], playsJSON);
+const result = statement(invoicesJSON[0]);
 const expected =
   '청구 내역 (고객명: BigCo)\n' +
   '  Hamlet: $650.00 (55석)\n' +
